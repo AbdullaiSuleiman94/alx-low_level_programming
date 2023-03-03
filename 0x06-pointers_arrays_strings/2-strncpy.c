98737195add20d3a8e2d1c3ff3073c2260f32664
AbdullaiSuleiman94 <abdullaisuley75@gmail.com>
2023-03-02 20:36:23 -0800

    Initial commit

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,23 +1,24 @@
 #include "main.h"
 /**
- * *_strcat - concatenates two strings
+ * *_strncpy - Copies a string
  * @dest: destination string
  * @src: origin string
+ * @n: integer
  * Return: Always 0
  */
-char *_strcat(char *dest, char *src)
+char *_strncpy(char *dest, char *src, int n)
 {
-	int len1, len2;
+	int i;
 
-	for (len1 = 0; dest[len1] != 0; len1++)
+	for (i = 0; i < n && src[i] != 0; i++)
 	{
+		dest[i] = src[i];
 	}
-	for (len2 = 0; src[len2] != 0; len2++)
+	while (i < n)
 	{
-		dest[len1] = src[len2];
-		len1++;
+		dest[i] = 0;
+		i++;
 	}
-	dest[len1] = 0;
 
 	return (dest);
 }
